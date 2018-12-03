1a27f2a7a730cf27f7050cec6940cef208a4ee03
Anthony Dempsey <adempsey@brocade.com>
2018-12-03 16:06:21 +0000

    Implement find_final_frequency method
    implemented the work horse of the program that takes a list of strings
    and adds them together to get the final frequency

--- day1/src/challenge1.py
+++ day1/src/challenge1.py
@@ -3,5 +3,11 @@
 def add_to_frequency(current, value):
     return current + int(value)
 
+def find_final_frequency(value_list):
+    current = 0
+    for value in value_list:
+        current = add_to_frequency(current, value)
+    return current
+
 if __name__ == "__main__":
     print("Hello, world")
